cfdec2681ab0e7433b81ebe408dfcbde32cef521
Jana Bobulis <71527795+JanaBobulis@users.noreply.github.com>
2022-01-16 13:38:00 +0000

    adding description title and image

--- index.js
+++ index.js
@@ -86,19 +86,98 @@ loadData();
 
 function createDestinationPage(data) {
   let test = document.getElementById('test');
-  if (test) {
-    test.innerHTML = `<h1>${data.crew[1].name}</h1>`
-  }
+  // if (test) {
+  //   test.innerHTML = `<h1>${data.crew[1].name}</h1>`
+  // }
   console.log(data); 
   console.log(data.destinations[0].images.png); 
 
   let destinationPage = document.getElementById('destination-content');
-  let moonImg = document.getElementById('moon-img')
+
   if(destinationPage) {
-    moonImg.innerHTML = `
+ 
+  let destinationImg = document.getElementById('destination-img');
+  let destinationLinks = document.getElementById('destinations');
+  let destinationName = document.querySelector('.destination-name')
+  let destinationLinksChildren = document.getElementById('destinations').children;
+  let destinationArray = Array.from(destinationLinksChildren)
+  console.log(destinationLinksChildren)
+
+    destinationImg.innerHTML = `
     <img src="${data.destinations[0].images.png}">
     `
-  }
+    destinationName.innerHTML = `
+    <h2>${data.destinations[0].name}</h2>
+    `
+
+    destinationArray.forEach((item) => {
+      console.log(item.dataset.item)
+      item.addEventListener('click', function(e) {
+        console.log(item.dataset.item)
+        // item.classList.add('active')
+        
+        if(item.dataset.item === "moon") {
+          destinationImg.innerHTML = `
+            <img src="${data.destinations[0].images.png}">
+          `
+          destinationName.innerHTML = `
+          <h2>${data.destinations[0].name}</h2>
+          `
   
+        } else if (item.dataset.item === "mars") {
+          destinationImg.innerHTML = `
+            <img src="${data.destinations[1].images.png}">
+          `
+          destinationName.innerHTML = `
+          <h2>${data.destinations[1].name}</h2>
+          `
+        } else if (item.dataset.item === "europa") {
+          destinationImg.innerHTML = `
+            <img src="${data.destinations[2].images.png}">
+          `
+          destinationName.innerHTML = `
+          <h2>${data.destinations[2].name}</h2>
+          `
+        }
+        else if (item.dataset.item === "titan") {
+          destinationImg.innerHTML = `
+            <img src="${data.destinations[3].images.png}">
+          `
+          destinationName.innerHTML = `
+          <h2>${data.destinations[3].name}</h2>
+          `
+        }
+      })
+    })
+    
+  }
 }
+
+  // destinationLinks.addEventListener('click', destinationClick);
+
+
+  
+  // function destinationClick(e) {
+  //   e.preventDefault();
+  //   console.log(destinationLinks.children[1].dataset.type)
+    // if (destinationLinks.children[0].dataset.type === "moon") {
+    //   console.log('moon')
+    // } else if (destinationLinks.children[1].dataset.type === "mars") {
+    //   console.log('mars')
+    // }
+  // }
+  
+  
+  
+
+  
+
+  
+  // if(destinationPage) {
+  //   moonImg.innerHTML = `
+  //   <img src="${data.destinations[0].images.png}">
+  //   `
+  // }
+  
+
 
